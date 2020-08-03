61ad61110f403b981f65ebacbebed121782c2c07
Yann Klein <yann.klein@me.com>
2020-08-03 21:37:49 +0900

    clapping eyelash feature

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ const changePicture = () => {
 }
 
 setInterval(changePicture, 5000);
+
+
+const openEyelash = (event) => {
+  console.log(window.scrollY);
+  const eyelash = document.querySelector(".eyelash-border");
+  const thres = window.innerHeight / 2;
+  if (window.scrollY > thres) {
+    const deg = (window.scrollY - thres) / 1.4 ** 2;
+    console.log(window.scrollY - thres, deg);
+    if (deg < 90) {
+      eyelash.style.transform = `translate(-50%, -50%) rotateX(${deg}deg)`;
+    } else {
+      eyelash.style.transform = `translate(-50%, -50%) rotateX(90deg)`;
+    }
+  }
+};
+
+document.addEventListener("scroll", openEyelash );
