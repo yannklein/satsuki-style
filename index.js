c6a1533756fb9df442a5b776700de4fa321cc6df
Yann Klein <yann.klein@me.com>
2020-08-04 00:19:38 +0900

    refactoring and cleaning

--- index.js
+++ index.js
@@ -12,35 +12,3 @@ const changePicture = () => {
 }
 
 setInterval(changePicture, 5000);
-
-// const secondEyelash = document.querySelector(".eyelash-second-part");
-// const secondPart = document.querySelector(".second-part");
-
-// const thirdEyelash = document.querySelector(".eyelash-third-part");
-// const thirdPart = document.querySelector(".third-part");
-
-// const openEyelash = (event) => {
-//   // Second part eyelash eyelash
-//   const thresSecond = - secondPart.getBoundingClientRect().top + (secondPart.offsetHeight / 2);
-//   if (thresSecond > 0) {
-//     const deg = thresSecond / 1.5 ** 2;
-//     if (deg < 90) {
-//       secondEyelash.style.transform = `translate(-50%, -50%) rotateX(${deg}deg)`;
-//     } else {
-//       secondEyelash.style.transform = `translate(-50%, -50%) rotateX(90deg)`;
-//     }
-//   }
-
-//   // Third part eyelash eyelash
-//   const thresThird = - thirdPart.getBoundingClientRect().top + (thirdPart.offsetHeight / 2);
-//   if (thresThird > 0) {
-//     const deg = thresThird / 1.5 ** 2;
-//     if (deg < 90) {
-//       thirdEyelash.style.transform = `translate(-50%, -50%) rotateX(${deg}deg)`;
-//     } else {
-//       thirdEyelash.style.transform = `translate(-50%, -50%) rotateX(90deg)`;
-//     }
-//   }
-// };
-
-// document.addEventListener("scroll", openEyelash );
